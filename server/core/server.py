e549b5597740f81cf8227b0d3a6c8faf240d64ef
daoductrung <trung@ddt.one>
2026-03-10 03:01:12 +0700

    Bump version to v0.1.7

--- server/core/server.py
+++ server/core/server.py
@@ -21,8 +21,8 @@ from ..messages.localization import Localization
 from ..documentation.manager import DocumentationManager
 
 
-VERSION = "0.1.6"
-LATEST_CLIENT_VERSION = "0.1.6"
+VERSION = "0.1.7"
+LATEST_CLIENT_VERSION = "0.1.7"
 UPDATE_URL = "https://github.com/Daoductrung/PlayAural/releases/latest/download/PlayAural.zip"
 UPDATE_HASH = "" # Optional SHA256
 
